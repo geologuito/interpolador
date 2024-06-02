d0b9676965114d70c46d7d3fb1ce201821209de3
Gonzalo Delgado <gonzadelgado.geo@gmail.com>
2024-06-02 14:50:04 -0300

    M-(r: mensaje ahora esta en una variable donde se valida si el resutado final es un numero)

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 function calcularDistanciaInterpolacion(cotaP1, cotaP2, distanciaP1P2, cotaInterpolacion) {
-    // Calcular la diferencia de cotas
-    let diferenciaCota = cotaP2 - cotaP1;
-  
-    // Calcular la razón entre la diferencia de cotas y la distancia entre los puntos
-    let razon = diferenciaCota / distanciaP1P2;
-  
-    // Calcular la distancia de interpolación
-    let distanciaInterpolacion = (cotaInterpolacion - cotaP1) / razon;
-  
-    return distanciaInterpolacion;
-  }
-  
-  function calcularYMostrarDistanciaInterpolacion() {
-    var cotaP1 = parseFloat(document.getElementById("cotaP1").value);
-    var cotaP2 = parseFloat(document.getElementById("cotaP2").value);
-    var distanciaP1P2 = parseFloat(document.getElementById("distanciaP1P2").value);
-    var cotaInterpolacion = parseFloat(document.getElementById("cotaInterpolacion").value);
-  
-    var distanciaInterpolacion = calcularDistanciaInterpolacion(cotaP1, cotaP2, distanciaP1P2, cotaInterpolacion);
-  
-    document.getElementById("resultado").innerHTML = "La distancia interpolada para una cota de " + cotaInterpolacion + " metros es " + distanciaInterpolacion + " centimetros (medir desde el punto1).";
-  }
-  
+  // Calcular la diferencia de cotas
+  let diferenciaCota = cotaP2 - cotaP1;
+
+  // Calcular la razón entre la diferencia de cotas y la distancia entre los puntos
+  let razon = diferenciaCota / distanciaP1P2;
+
+  // Calcular la distancia de interpolación
+  let distanciaInterpolacion = (cotaInterpolacion - cotaP1) / razon;
+
+  return distanciaInterpolacion;
+}
+
+function calcularYMostrarDistanciaInterpolacion() {
+  let cotaP1 = parseFloat(document.getElementById("cotaP1").value);
+  let cotaP2 = parseFloat(document.getElementById("cotaP2").value);
+  let distanciaP1P2 = parseFloat(document.getElementById("distanciaP1P2").value);
+  let cotaInterpolacion = parseFloat(document.getElementById("cotaInterpolacion").value);
+
+  let distanciaInterpolacion = calcularDistanciaInterpolacion(cotaP1, cotaP2, distanciaP1P2, cotaInterpolacion);
+
+  //verifico que no haya inputs vacios
+  let mensaje = (!isNaN(distanciaInterpolacion))
+    ? `La distancia interpolada para una cota de ${cotaInterpolacion} metros es  ${distanciaInterpolacion} centimetros (midiendo desde el punto1).`
+    : `por favor ingrese los datos correctamente`
+
+  document.getElementById("resultado").innerHTML = mensaje
+
+}
